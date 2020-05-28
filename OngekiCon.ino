fe94d45b4caa8fd8951500fdff3d6b891ac147fb
waterglass32 <64804079+waterglass32@users.noreply.github.com>
2020-05-28 15:19:20 -0700

    Add files via upload

--- OngekiCon.ino
+++ OngekiCon.ino
@@ -1,43 +1,64 @@
 /* Code based off example code provided by David Madison's GamepadPins example included in the Xinput Library
- * 
- * Before you can use this code, make sure to download needed libraries
- * - https://github.com/dmadison/ArduinoXInput
- * - https://github.com/FastLED/FastLED
- * 
- * You will also need to add this to your arduino IDE's hardware folder
- * - https://github.com/dmadison/ArduinoXInput_AVR
- * 
- * This file was created with Arduino Leonardo board in mind
- * 
- * USAGE:
- * this will cover your basic XInput controller for ONGEKI
- * 
- * ONGEKI's XInput lever is processed as when neither trigger is pressed, your character is in the center of the screen.
- * Press the left side and it moves left, release and it returns to center. Same with the right side
- * 
- * I set the lever range to 519 which is an approximate halfway point between the min and max values of my potentiometer.
- * That half way point has to be calculated because you're splitting one input method between two "triggers"
- * I also set the lever calculations to work around that half way point.
- * 
- * Lights use a ws2812b strip. This currently only has 12 lights programmed. 3 for each side button and 2 sets of button lights
- * the lights are set to always be on as segatools does not yet support HID for ONGEKI
- */
 
-#include <XInput.h>
-#include <FastLED.h>
+   Before you can use this code, make sure to download needed libraries
+   - https://github.com/dmadison/ArduinoXInput
+   - https://github.com/FastLED/FastLED
+
+   You will also need to add this to your arduino IDE's hardware folder
+   - https://github.com/dmadison/ArduinoXInput_AVR
+
+   This file was created with Arduino Leonardo board in mind
+
+   USAGE:
+   this will cover your basic XInput controller for ONGEKI
+
+   ONGEKI's XInput lever is processed as when neither trigger is pressed, your character is in the center of the screen.
+   Press the left side and it moves left, release and it returns to center. Same with the right side
+
+   I set the lever range to 519 which is an approximate halfway point between the min and max values of my potentiometer.
+   That half way point has to be calculated because you're splitting one input method between two "triggers"
+   I also set the lever calculations to work around that half way point.
+
+   Lights use a ws2812b strip. This currently only has 12 lights programmed. 3 for each side button and 2 sets of button lights
+   the lights are set to always be on as segatools does not yet support HID for ONGEKI
+
+   Note: Removing XInput in place of keyboard and mouse
+   Also removed leftmenu and rightmenu pins because my con does not include those buttons
+
+   May add FastLED support later (I commented it out)
+*/
+
+//#include <XInput.h>
+//#include <FastLED.h>
+
+#include <Keyboard.h>
+#include <Mouse.h>
+
 
 // LED Strip Setup
-#define LED_PIN     12
-#define NUM_LEDS    12
-CRGB leds[NUM_LEDS];
+//#define LED_PIN     12
+//#define NUM_LEDS    12
+//CRGB leds[NUM_LEDS];
 
 // Lever Setup
-int leverRange = 519;//519
+
+
+
+
+int leverRange = 519; //519
 int positionA = 0;
 int positionB = 0;
+int previousPosition = 0;
+int currentPosition = 0;
 
 // Lever Pins
 const int Pin_Lever = A0;
+
+
+// Button Setup
+bool buttonPressed[] = {false, false, false, false, false, false, false, false};
+bool recentState[] = {false, false, false, false, false, false, false, false};
+const char keyBinds[] = {'s', 'd', 'f', 'j', 'k', 'l', 'a', ';'};
 
 // Button Pins
 const int Pin_LeftA = 2;
@@ -48,16 +69,16 @@ const int Pin_RightB = 6;
 const int Pin_RightC = 7;
 const int Pin_LeftSide = 8;
 const int Pin_RightSide = 9;
-const int Pin_LeftMenu = 10;
-const int Pin_RightMenu = 11;
+//const int Pin_LeftMenu = 10;
+//const int Pin_RightMenu = 11;
 
 void setup() {
-  // LED Setup
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  //  // LED Setup
+  //  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
 
-  // Set Lever Range
-  XInput.setTriggerRange(0, leverRange);
-  
+  //  // Set Lever Range
+  //  XInput.setTriggerRange(0, leverRange);
+
   // Set buttons as inputs, using internal pull-up resistors
   pinMode(Pin_LeftA, INPUT_PULLUP);
   pinMode(Pin_LeftB, INPUT_PULLUP);
@@ -67,28 +88,33 @@ void setup() {
   pinMode(Pin_RightC, INPUT_PULLUP);
   pinMode(Pin_LeftSide, INPUT_PULLUP);
   pinMode(Pin_RightSide, INPUT_PULLUP);
-  pinMode(Pin_LeftMenu, INPUT_PULLUP);
-  pinMode(Pin_RightMenu, INPUT_PULLUP);
-  XInput.setAutoSend(false);  // Wait for all controls before sending
-  XInput.begin();
+//  pinMode(Pin_LeftMenu, INPUT_PULLUP);
+//  pinMode(Pin_RightMenu, INPUT_PULLUP);
+  //  XInput.setAutoSend(false);  // Wait for all controls before sending
+  //  XInput.begin();
+  Keyboard.begin();
+  Mouse.begin();
 }
 
 void loop() {
   // LED Colors
-  for(int i = 0; i < 3; ++i){
-    leds[i] = CRGB(255, 20, 255);
-  }  
-  leds[3] = CRGB::Red;
-  leds[4] = CRGB::Green;
-  leds[5] = CRGB::Blue;
-  leds[6] = CRGB::Red;
-  leds[7] = CRGB::Green;
-  leds[8] = CRGB::Blue;
-  for(int i = 9; i < 12; ++i){
-    leds[i] = CRGB(255, 20, 255);
-  } 
-  FastLED.show();
-  
+  //  for(int i = 0; i < 3; ++i){
+  //    leds[i] = CRGB(255, 20, 255);
+  //  }
+  //  leds[3] = CRGB::Red;
+  //  leds[4] = CRGB::Green;
+  //  leds[5] = CRGB::Blue;
+  //  leds[6] = CRGB::Red;
+  //  leds[7] = CRGB::Green;
+  //  leds[8] = CRGB::Blue;
+  //  for(int i = 9; i < 12; ++i){
+  //    leds[i] = CRGB(255, 20, 255);
+  //  }
+  //  FastLED.show();
+
+
+
+
   // Read pin values and store in variables
   int lever = analogRead(Pin_Lever);
   boolean leftA = !digitalRead(Pin_LeftA);
@@ -99,35 +125,101 @@ void loop() {
   boolean rightC = !digitalRead(Pin_RightC);
   boolean leftSide = !digitalRead(Pin_LeftSide);
   boolean rightSide = !digitalRead(Pin_RightSide);
-  boolean leftMenu  = !digitalRead(Pin_LeftMenu);
-  boolean rightMenu = !digitalRead(Pin_RightMenu);
+  //  boolean leftMenu  = !digitalRead(Pin_LeftMenu);
+  //  boolean rightMenu = !digitalRead(Pin_RightMenu);
 
-  // Set XInput DPAD values and allow simultaneous opposite direction pressing
-  XInput.setDpad(leftB, leftB, leftA, leftC, false);// leftB listed twice because I needed something to fill that second dpad slot..it's unused in game
-  
-  // Set XInput buttons
-  XInput.setButton(BUTTON_X, rightA);
-  XInput.setButton(BUTTON_Y, rightB);
-  XInput.setButton(BUTTON_B, rightC);
-  XInput.setButton(BUTTON_LB, leftSide);
-  XInput.setButton(BUTTON_RB, rightSide);
-  XInput.setButton(BUTTON_BACK, leftMenu);
-  XInput.setButton(BUTTON_START, rightMenu);
+  //  // Set XInput DPAD values and allow simultaneous opposite direction pressing
+  //  XInput.setDpad(leftB, leftB, leftA, leftC, false);// leftB listed twice because I needed something to fill that second dpad slot..it's unused in game
 
-  // Set XInput trigger values
-  XInput.setTrigger(TRIGGER_LEFT, positionA);
-  XInput.setTrigger(TRIGGER_RIGHT, positionB);
+  //  Lever Calibration
+  boolean leverCalibrated = false;
 
-  // Calculate lever values
-  if(lever < leverRange){
-    positionB = -(lever-leverRange);
-  }else if( lever > leverRange){
-    positionA = lever-leverRange;
-  }else{
-    positionA = 0;
-    positionB = 0;
+  while leverCalibrated == false{    //Reminder: Test to see if the value returned is an integer or a float
+  leftA = !digitalRead(Pin_LeftA);
+    if leftA == true{
+    lever = analogRead(Pin_Lever);
+      int leverMaxLeft = lever;
+      while leverCalibrated == false{
+      rightA = !digitalRead(Pin_RightA);
+        if rightA == true{
+        lever = analogRead(Pin_Lever);
+          int leverMaxRight = lever;
+          leverCalibrated = true;
+        }
+      }
+    }
   }
-  
-  // Send control data to the computer
-  XInput.send();
+
+  leverRange = (leverMaxLeft + leverMaxRight) / 2;
+
+  //Main loop
+  while (1) {
+    lever = analogRead(Pin_Lever);
+    leftA = !digitalRead(Pin_LeftA);
+    leftB = !digitalRead(Pin_LeftB);
+    leftC = !digitalRead(Pin_LeftC);
+    rightA = !digitalRead(Pin_RightA);
+    rightB = !digitalRead(Pin_RightB);
+    rightC = !digitalRead(Pin_RightC);
+    leftSide = !digitalRead(Pin_LeftSide);
+    rightSide = !digitalRead(Pin_RightSide);
+    
+    // Calculate lever values
+    if (lever < leverRange) {
+      positionB = -(lever - leverRange);
+    } else if ( lever > leverRange) {
+      positionA = lever - leverRange;
+    } else {
+      positionA = 0;
+      positionB = 0;
+    }
+
+    currentPosition = positionA + positionB;
+
+    if previousPosition != currentPosition{
+      Mouse.move(currentPosition - previousPosition);
+      previousPosition = currentPosition;
+    }
+
+    buttonPressed[] = {leftA, leftB, leftC, rightA, rightB, rightC, leftSide, rightSide};
+
+    for (int i = 0; i < 8; i++)
+    {
+      if recentState[i] != buttonPressed[i]
+      {
+        if buttonPressed[i] == true
+        {
+          Keyboard.press(keyBinds[i]);
+        }
+        else
+        {
+          Keyboard.release(keyBinds[i]);
+        }
+      }
+    }
+
+    recentState[] = buttonPressed[];
+
+                    //  // Set XInput buttons
+                    //  XInput.setButton(BUTTON_X, rightA);
+                    //  XInput.setButton(BUTTON_Y, rightB);
+                    //  XInput.setButton(BUTTON_B, rightC);
+                    //  XInput.setButton(BUTTON_LB, leftSide);
+                    //  XInput.setButton(BUTTON_RB, rightSide);
+                    //  XInput.setButton(BUTTON_BACK, leftMenu);
+                    //  XInput.setButton(BUTTON_START, rightMenu);
+
+
+
+
+                    //  // Set XInput trigger values
+                    //  XInput.setTrigger(TRIGGER_LEFT, positionA);
+                    //  XInput.setTrigger(TRIGGER_RIGHT, positionB);
+
+
+
+
+                    //  // Send control data to the computer
+                    //  XInput.send();
+  }
 }
